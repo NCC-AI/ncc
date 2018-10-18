65184188a180751c259426bcc157a26a0ce53d62
aiorhiroki <1234defgsigeru@gmail.com>
2018-10-18 20:57:17 +0900

    add comment

--- ncc/models/conv3d.py
+++ ncc/models/conv3d.py
@@ -6,8 +6,10 @@ import numpy as np
 from .util import inst_layers
 
 
-# Convolution 3D layer
-def Conv(filters=16, kernel_size=(3, 3, 3), activation='relu', input_shape=None):
+def Conv(filters, kernel_size=(3, 3, 3), activation='relu', input_shape=None):
+    """
+    # Convolution 3D layer
+    """
     if input_shape:
         return Conv3D(filters=filters,
                       kernel_size=kernel_size,
@@ -21,8 +23,13 @@ def Conv(filters=16, kernel_size=(3, 3, 3), activation='relu', input_shape=None)
                       activation=activation)
 
 
-# Define Model
 def conv3d(input_dim, num_classes):
+    """
+    # Define Model
+    # Arguments
+        input_dim: (depth, width, height, channel)
+        num_classes: number of classes
+    """
 
     small_size = min(input_dim[:3])
     nb_convolution = 0
@@ -30,8 +37,6 @@ def conv3d(input_dim, num_classes):
     while small_size > 1:
         small_size = small_size // 2
         nb_convolution += 1
-
-    latent_dim = np.prod(input_dim[:3]) * (8*2**(nb_convolution-1)) // (8**nb_convolution) // 4
 
     layers = [
         Conv(8, input_shape=input_dim),
@@ -46,6 +51,11 @@ def conv3d(input_dim, num_classes):
         ]
         for layer_id in range(1, nb_convolution)
     ]
+
+    latent_dim = np.prod(input_dim[:3])  # depth * width * height
+    latent_dim *= 8 * 2 ** (nb_convolution - 1)  # filter size at last convolution
+    latent_dim //= 8 ** nb_convolution  # number of training parameters reduced with convolution
+    latent_dim //= 4  # dimension reduction from flatten to dense
 
     layers += [
         Flatten(),
