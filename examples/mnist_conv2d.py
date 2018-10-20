3590465efa8fab67f190fa92d279cedf869c24b3
aiorhiroki <1234defgsigeru@gmail.com>
2018-10-20 23:17:53 +0900

    refactoring

--- examples/mnist_conv2d.py
+++ examples/mnist_conv2d.py
@@ -6,30 +6,26 @@ from keras.datasets import mnist
 
 import numpy as np
 
+# parameters
 num_classes = 10
-depth = 8
-width, height = 28, 28
-channel = 1
-input_shape = (width, height, channel)
-epochs = 30
+input_shape = (28, 28, 1)
+epochs = 2
 batch_size = 128
 
+# prepare data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 x_train = np.expand_dims(x_train, axis=3)
 x_test = np.expand_dims(x_test, axis=3)
-
 x_train, y_train = preprocess_input(x_train, y_train)
 x_test, y_test = preprocess_input(x_test, y_test)
-
-
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
+# build and train model
 model = Model2D(input_shape=input_shape, num_classes=num_classes)
 model.summary()
-
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
 history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size)
 
+# save history and model
 save_history(history)
+model.save_weights('cnn_2d_model.h5')
