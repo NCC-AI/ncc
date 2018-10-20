c427bb797b1626ba82c5f2d22056c8a97207f5af
goひろき <1234defgsigeru@gmail.com>
2018-10-20 09:21:35 +0900

    Update mnist_conv3d.py

--- examples/mnist_conv3d.py
+++ examples/mnist_conv3d.py
@@ -1,4 +1,4 @@
-from ncc.models import conv3d
+from ncc.models import Model3D
 from ncc.history import save_history
 from keras.datasets import mnist
 import numpy as np
@@ -7,7 +7,7 @@ num_classes = 10
 depth = 8
 width, height = 28, 28
 channel = 1
-input_dim = (depth, width, height, channel)
+input_shape = (depth, width, height, channel)
 epochs = 30
 batch_size = 128
 
@@ -15,8 +15,8 @@ batch_size = 128
 
 x_train = np.asarray(x_train, dtype='float32') / 255.
 x_test = np.asarray(x_test, dtype='float32') / 255.
-x_train = np.reshape(x_train, (len(x_train),)+input_dim[1:])
-x_test = np.reshape(x_test, (len(x_test),)+input_dim[1:])
+x_train = np.reshape(x_train, (len(x_train),)+input_shape[1:])
+x_test = np.reshape(x_test, (len(x_test),)+input_shape[1:])
 
 x_train_3d = np.asarray([x_train for _ in range(depth)]).transpose((1, 0, 2, 3, 4))
 x_test_3d = np.asarray([x_test for _ in range(depth)]).transpose((1, 0, 2, 3, 4))
@@ -28,7 +28,7 @@ y_test = np.eye(num_classes, dtype='float32')[y_test]
 
 print(x_train_3d.shape, y_train.shape, x_test_3d.shape, y_test.shape)
 
-model = conv3d(input_dim=input_dim, num_classes=num_classes)
+model = Model3D(input_shape=input_shape, num_classes=num_classes)
 model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
