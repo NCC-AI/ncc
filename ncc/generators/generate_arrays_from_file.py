3a2e3de826d94941a313e1ebd1cd01fbc8cea9b2
aiorhiroki <1234defgsigeru@gmail.com>
2018-11-10 12:50:49 +0900

    generalized generator

--- ncc/generators/generate_arrays_from_file.py
+++ ncc/generators/generate_arrays_from_file.py
@@ -1,55 +1,46 @@
 import numpy as np
+import re
 
 from keras.preprocessing.image import load_img, img_to_array
 
 
-def set_input_data(image_path, class_name, class_num, dimension, nb_frames=32):
-    if dimension == 3:
-        image = []
-        first_image_path = image_path.split('/')
-        image_dir = first_image_path[:-1]
-        first_frame_index = int(first_image_path[-1].replace('.jpg', ''))
-        for i in range(nb_frames):
-            frame_index = first_frame_index + i
-            frame_image_path = image_dir + [str(frame_index)]
-            imaga_path = '/'.join(frame_image_path) + '.jpg'
-            frame = load_img(image_path, target_size=(256, 256))
-            array = img_to_array(frame)
-            frame = array/255.
-            image.append(frame)
-        image = np.asanyarray(image)
-        class_one_hot = np.eye(class_num)[int(class_name)-1]
-    else:
-        image = load_img(image_path, target_size=(299, 299))
-        array = img_to_array(image)
-        image = array/255.
-        class_one_hot = np.eye(class_num)[int(class_name)-1]
-    return image, class_one_hot
-
-
-def generate_arrays_from_file(annotation_data, mode, dimension, batch_size, nb_classes):
+def generate_arrays_from_annotation(annotation_data, dimension, batch_size, nb_classes, target_size):
     while True:
+
         x, y = [], []
         i = 0
-        if mode == 'train':
-            np.random.shuffle(annotation_data)
-            for index, label_feature in enumerate(annotation_data):
-                feature, label = set_input_data(label_feature[0], label_feature[1], nb_classes, dimension)
-                y.append(label)
-                x.append(feature)
-                i += 1
-                if i == batch_size:
-                    yield (np.array(x), np.array(y))
-                    i = 0
-                    x, y = [], []
-        if mode == 'test':
-            np.random.shuffle(annotation_data)
-            for index, label_feature in enumerate(annotation_data):
-                feature, label = set_input_data(label_feature[0], label_feature[1], nb_classes, dimension)
-                y.append(label)
-                x.append(feature)
-                i += 1
-                if i == batch_size:
-                    yield (np.array(x), np.array(y))
-                    i = 0
-                    x, y = [], []
+
+        np.random.shuffle(annotation_data)
+        for index, label_feature in enumerate(annotation_data):
+            feature = _set_input_data(label_feature[0], target_size, dimension)
+            y.append(label_feature[1])
+            x.append(feature)
+            i += 1
+            if i == batch_size:
+                yield (np.array(x), np.eye(nb_classes)[np.array(y)])
+                i = 0
+                x, y = [], []
+
+
+def _set_input_data(image_path, target_size, dimension, nb_frames=32):
+    # color order: (R, G, B)
+    if dimension == 2:
+        image = load_img(image_path, target_size)
+        array = img_to_array(image)
+
+    # stack sequence image with index (e.g. from image-4.jpg to image-36.jpg)
+    elif dimension == 3:
+        array = []
+        first_frame_index = int(re.findall('(\d+)', image_path)[-1])
+
+        for frame_index in range(first_frame_index, first_frame_index + nb_frames):
+            sequence_path = image_path.replace(str(first_frame_index), str(frame_index))
+            frame = load_img(sequence_path, target_size)
+            array.append(img_to_array(frame))
+
+        array = np.asarray(array)
+
+    else:
+        raise ValueError('invalid dimension arg. dimension should be 2 or 3')
+
+    return array
