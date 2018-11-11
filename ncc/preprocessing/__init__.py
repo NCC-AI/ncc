752f28049d7fcf69f6f5090698f78e9f2f0b1e61
yumion <yumion28@gmail.com>
2018-11-11 09:54:54 +0900

    __init__書き忘れてました

--- ncc/preprocessing/__init__.py
+++ ncc/preprocessing/__init__.py
@@ -1,3 +1,4 @@
 from .preprocessing import preprocess_input
 from .remove_black_background import remove_black_background
 from .segmentation_input import segmentation_input
+from .get_dataset import get_dataset
