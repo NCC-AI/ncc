585136e4436180cc0a018e9c9a2d5cf12054be4d
yumion <yumion28@gmail.com>
2018-11-15 15:37:50 +0900

    リファクタ

--- ncc/preprocessing/get_dataset.py
+++ ncc/preprocessing/get_dataset.py
@@ -9,11 +9,11 @@ def get_dataset(target_dir):
     file_list = glob.glob(target_dir + '/*/')
     # image load
     x_array, y_array = [], []
-    for num in range(len(file_list)):
-        for picture in list_pictures(file_list[num]):
+    for class_index, folder_name in enumerate(file_list):
+        for picture in list_pictures(folder_name):
             img = img_to_array(load_img(picture))
             x_array.append(img) # input image
-            y_array.append(num) # label
+            y_array.append(class_index) # label
     x_array = np.asarray(x_array)
     y_array = np.asarray(y_array)
     return x_array, y_array
