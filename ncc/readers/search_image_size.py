ca573a10b6e166d44d67ff5418a3faa6d916bcae
aiorhiroki <1234defgsigeru@gmail.com>
2018-11-17 23:10:41 +0900

    add search img size func from annotation file

--- ncc/readers/search_image_size.py
+++ ncc/readers/search_image_size.py
@@ -1,18 +1,29 @@
 from glob import glob
 import cv2
 import numpy as np
+import pandas as pd
 
 
-def search_image_size(target_dir):
+def search_from_dir(target_dir):
     files = []
     for image_suffix in ['jpg', 'png', 'jpeg', 'JPG', 'PNG', 'JPEG']:
-        image_paths = target_dir + '/*/*/*.' + image_suffix  # target_dir/trained/class_name/*.jpg
+        image_paths = target_dir + '/*/*/*.' + image_suffix  # target_dir/train(test)/class_name/*.jpg
         files += glob(image_paths)
 
-    return median_size(files)
+    return _median_size(files)
 
 
-def median_size(files):
+def search_from_annotation(annotation_file):
+    annotation_df = pd.read_csv(annotation_file).values.T
+    files = annotation_df[0]
+    class_index = annotation_df[1]
+
+    class_names = np.unique(class_index)
+
+    return _median_size(files), class_names
+
+
+def _median_size(files):
     if len(files) > 10000:
         files = files[:10000]  # ignore large image files
 
@@ -22,7 +33,7 @@ def median_size(files):
         height, width = image.shape[:2]
         height_list.append(height)
         width_list.append(width)
-    height_median = np.median(height_list)
-    width_median = np.median(width_list)
+    height_median = np.median(height_list).astype('int')
+    width_median = np.median(width_list).astype('int')
 
     return height_median, width_median
