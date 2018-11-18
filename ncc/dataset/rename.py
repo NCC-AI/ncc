1b68d3929ab7230c575c67f73ed165920d07d91b
takano <9takanotsume9@gmail.com>
2018-11-18 18:00:45 +0900

    modified

--- ncc/dataset/rename.py
+++ ncc/dataset/rename.py
@@ -3,7 +3,11 @@ import glob
 import xml.etree.ElementTree as ET
 
 
-def rename(dirpath, prefix):
+def get_zero_padding(filelist):
+    return '_%0'+str(len(str(len(filelist))))+'d'
+
+
+def rename(dirpath, prefix, start=1):
     """ Rename files for easy to understand.
     # Arguments
         dirpath: Path to the directory.
@@ -13,16 +17,16 @@ def rename(dirpath, prefix):
         New name files, 'aaa_000b.ext'. (aaa=prefix, b=serial number, .ext=extension)
     """
 
-    names = [x for x in sorted(os.listdir(dirpath)) if not x == '.DS_Store']
+    names = [x for x in sorted(os.listdir(dirpath)) if os.path.isfile(os.path.join(dirpath, x))]
     ext = list(set([os.path.splitext(x)[1] for x in names]))
     assert len(ext) == 1, 'It contains multiple extensions.'
 
-    new_name = str(prefix) + '_%0' + str(len(str(len(names)))) + 'd' + str.lower(ext[0])
-    for idx, name in enumerate(names, start=1):
+    new_name = str(prefix) + get_zero_padding(names) + str.lower(ext[0])
+    for idx, name in enumerate(names, start=start):
         os.rename(os.path.join(dirpath, name), os.path.join(dirpath, new_name) % idx)
 
 
-def rename_with_xml(xml_dir, img_dir, prefix):
+def rename_with_xml(xml_dir, img_dir, prefix, start=1):
     """ Rename image files and xml files.
     # Arguments
         xml_dir: Path to the directory that contains xml.
@@ -32,18 +36,18 @@ def rename_with_xml(xml_dir, img_dir, prefix):
     # Returns
         New name files, 'aaa_000b.ext'. (aaa=prefix, b=serial number, .ext=extension)
     """
-    xml_names = [x for x in sorted(os.listdir(xml_dir)) if not x == '.DS_Store']
-    img_names = [x for x in sorted(os.listdir(img_dir)) if not x == '.DS_Store']
+    xml_names = [x for x in sorted(os.listdir(xml_dir)) if x.endswith(('xml',))]
+    img_names = [x for x in sorted(os.listdir(img_dir)) if x.endswith(('png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG'))]
     xml_ext = list(set([os.path.splitext(x)[1] for x in xml_names]))
     img_ext = list(set([os.path.splitext(x)[1] for x in img_names]))
     assert len(xml_ext) == 1, 'Xml directory contains multiple extensions.'
     assert len(img_ext) == 1, 'Image directory contains multiple extensions.'
     assert len(xml_names) == len(img_names), 'Mismatch length, %d xmls with %d images.' % (len(xml_names), len(img_names))
 
-    new_img = str(prefix) + '_%0' + str(len(str(len(img_names)))) + 'd' + str.lower(img_ext[0])
-    new_xml = str(prefix) + '_%0' + str(len(str(len(xml_names)))) + 'd' + str.lower(xml_ext[0])
+    new_img = str(prefix) + get_zero_padding(img_names) + str.lower(img_ext[0])
+    new_xml = str(prefix) + get_zero_padding(xml_names) + str.lower(xml_ext[0])
  
-    for idx, xml in enumerate(xml_names, start=1):
+    for idx, xml in enumerate(xml_names, start=start):
         tree = ET.parse(os.path.join(xml_dir, xml))
         root = tree.getroot()
         img = root.find('filename').text
@@ -54,3 +58,30 @@ def rename_with_xml(xml_dir, img_dir, prefix):
         tree.write(os.path.join(xml_dir, xml))
         # Rename xml file
         os.rename(os.path.join(xml_dir, xml), os.path.join(xml_dir, new_xml) % idx)
+
+
+def change_filename(xml_dir):
+    """ Change path tag in the xml.
+    """
+    xmls = sorted([x for x in os.listdir(xml_dir) if x.endswith(('xml',))])
+
+    for xml in xmls:
+        tree = ET.parse(os.path.join(xml_dir, xml))
+        root = tree.getroot()
+        filename = root.find('filename').text
+        filename = filename + '.jpg'
+        root.find('filename').text = filename
+        tree.write(os.path.join(xml_dir, xml))
+
+
+def change_classname(xml_dir):
+    """ Change path tag in the xml.
+    """
+    xmls = sorted([x for x in os.listdir(xml_dir) if x.endswith(('xml',))])
+
+    for xml in xmls:
+        tree = ET.parse(os.path.join(xml_dir, xml))
+        root = tree.getroot()
+        for object_tree in root.findall('object'):
+            object_tree.find('name').text = 'scc'
+        tree.write(os.path.join(xml_dir, xml))
