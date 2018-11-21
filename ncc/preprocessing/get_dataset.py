6699fe44cf0e00a2a891fbf77297d9e23eb598e9
Murata <4318571@ed.tus.ac.jp>
2018-11-21 15:06:43 +0900

    異なるサイズの画像ファイルでも読み込めるようにしました

--- ncc/preprocessing/get_dataset.py
+++ ncc/preprocessing/get_dataset.py
@@ -1,19 +1,25 @@
 import numpy as np
 import glob
 
+from PIL import Image
 from keras_preprocessing.image import array_to_img, img_to_array, list_pictures, load_img
-
+from ncc.readers.search_image_size import search_from_dir
 
 def get_dataset(target_dir):
     # get dataset from target_dir
     file_list = glob.glob(target_dir + '/*/')
     # image load
     x_array, y_array = [], []
+    height_median, width_median = search_from_dir(target_dir) #get median size
+
     for class_index, folder_name in enumerate(file_list):
         for picture in list_pictures(folder_name):
-            img = img_to_array(load_img(picture))
-            x_array.append(img) # input image
+            img = load_img(picture) #img type = PIL.image
+            img_resize = img.resize((width_median ,height_median), Image.LANCZOS) #Image.resize((width, height),Filter_type)
+            img_array = img_to_array(img_resize) #np.array
+            x_array.append(img_array) # input image
             y_array.append(class_index) # label
+
     x_array = np.asarray(x_array)
     y_array = np.asarray(y_array)
     return x_array, y_array
