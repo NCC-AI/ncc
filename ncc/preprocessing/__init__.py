ff6700fa6bd530d34e083bdc758ef5fc163ff48b
aiorhiroki <1234defgsigeru@gmail.com>
2019-03-03 15:06:39 +0900

    add init

--- ncc/preprocessing/__init__.py
+++ ncc/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .preprocessing import preprocess_input
 from .remove_black_background import remove_black_background
 from .segmentation_input import segmentation_input
-from .get_dataset import get_dataset
+from .get_dataset import get_dataset, list_files
